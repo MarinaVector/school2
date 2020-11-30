82e0d4980fbd934a9715b03fbabb38e56b805805
beluana@mail.ru <vehksrf5577>
2020-12-01 00:13:50 +0500

    frontend progress coaching-2 page

--- general-components/nav.php
+++ general-components/nav.php
@@ -3,9 +3,9 @@
         <img class="main-nav__nav-logo" src="img/Logo.png" alt="Логотип онлайн школы">
     </div>
     <div class="main-nav__nav-links mt-4 mx-auto">
-        <a class="main-nav__nav-link nav-link">Об авторе</a>
-        <a class="main-nav__nav-link nav-link" href="coaching.php">Коучинг</a>
-        <a class="main-nav__nav-link nav-link">Обучение</a>
+        <a class="main-nav__nav-link nav-link"  href="#">Об авторе</a>
+        <a class="main-nav__nav-link nav-link" href="../coaching_life.php">Коучинг</a>
+        <a class="main-nav__nav-link nav-link" href="#">Обучение</a>
     </div>
     <div class="main-nav__nav-links nav-link mt-md-3 mb-md-0 mb-4">
         <div class="d-inline">
