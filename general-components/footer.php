52f102be52d645eb770b8cf2ea83227b5d5a3443
beluana@mail.ru <vehksrf5577>
2021-04-19 22:57:30 +0500

    progress

--- general-components/footer.php
+++ general-components/footer.php
@@ -1,20 +1,21 @@
 <footer class="footer container-fluid pt-3 pb-5">
-    <div class="container">
-    <nav class="nav nav-fill col-md-12">
-        <a class="nav-link" href="../index.php">
+
+    <nav class="container px-0">
+        <div class="row">
+        <a class="col-lg-2 mx-lg-0 col-12 text-center" href="../index.php">
             <img class="main-nav__nav-logo" src="img/Small-logo.png" alt="Логотип бизнес монетизатор">
         </a>
 
-        <div class="main-nav__nav-links mb-3 mt-4">
-            <a class="main-nav__nav-link nav-link nav_small" href="#">Об авторе</a>
-            <a class="main-nav__nav-link nav-link nav_small" href="/courses.php">Курсы</a>
-            <a class="main-nav__nav-link nav-link nav_small" href="#">Консультация</a>
-            <a class="main-nav__nav-link nav-link nav_small" href="#">Отзывы</a>
+        <div class="main-nav__footer-links d-flex justify-content-between col-12 col-lg-6 offset-lg-1 mb-3 mt-4">
+            <a class="main-nav__footer-link nav_small" href="#">Об авторе</a>
+            <a class="main-nav__footer-link nav_small" href="/courses.php">Курсы</a>
+            <a class="main-nav__footer-link nav_small" href="#">Консультация</a>
+            <a class="main-nav__footer-link nav_small" href="#">Отзывы</a>
         </div>
 
-        <div class="main-nav__nav-links nav-link btn-link mt-3">
+        <div class="col-12 text-center col-lg-2 offset-lg-1 btn-link mt-4 mb-sm-3 px-0">
             <div class="d-inline">
-                <img class="mr-1" src="../img/Whatsapp%20.png"
+                <img class="mr-3" src="../img/Whatsapp%20.png"
                      alt="Sosial-icon">
             </div>
 
@@ -22,10 +23,11 @@
                 <a class="main-nav__nav-simple py-2 mb-2 mb-md-0" href="auth.php">Задать вопрос</a>
             </div>
         </div>
-    </nav>
-    <div class="mt-1">
+
+    <div class="mt-1 col-12 col-xl-3 mx-auto">
         <p class="text-center footer__footer-copyright">Все права защищены</p>
     </div>
-    </div>
+        </div>
+    </nav>
 </footer>
 
